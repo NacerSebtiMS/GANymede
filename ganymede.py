d569982c14a59802ae824c6b8bf229168e9193d7
Nacer Sebti <nacersebti.ms@gmail.com>
2019-07-04 17:59:48 -0400

    Added wgan-gp loss

--- ganymede.py
+++ ganymede.py
@@ -10,17 +10,17 @@ import tensorflow as tf
 
 import numpy as np
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-import sys,os,shutil
+#import sys,os,shutil
 
-from keras.datasets import mnist
-from keras.models import Sequential, Model, load_model
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Lambda
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras import backend as K
+from keras.models import Sequential, Model#, load_model
+from keras.layers import Input, Dense, Reshape, Flatten, Lambda
+from keras.layers import BatchNormalization, Activation
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling1D, Conv1D
-from keras.optimizers import Adam
+#from keras.optimizers import Adam
 # =============================
 
 # ========== Files ==========
@@ -258,6 +258,25 @@ def train_wavegan(epochs, batch_size, save_interval):
 # ================================
     
 # ========== Loss function ==========
+# Source : https://github.com/keras-team/keras-contrib/blob/master/examples/improved_wgan.py
+def gradient_penalty_loss(y_true, y_pred, averaged_samples,gradient_penalty_weight):
+	# first get the gradients:
+    #   assuming: - that y_pred has dimensions (batch_size, 1)
+    #             - averaged_samples has dimensions (batch_size, nbr_features)
+    # gradients afterwards has dimension (batch_size, nbr_features), basically
+    # a list of nbr_features-dimensional gradient vectors
+    gradients = K.gradients(y_pred, averaged_samples)[0]
+    # compute the euclidean norm by squaring ...
+    gradients_sqr = K.square(gradients)
+    #   ... summing over the rows ...
+    gradients_sqr_sum = K.sum(gradients_sqr,
+                              axis=np.arange(1, len(gradients_sqr.shape)))
+    #   ... and sqrt
+    gradient_l2_norm = K.sqrt(gradients_sqr_sum)
+    # compute lambda * (1 - ||grad||)^2 still for each single sample
+    gradient_penalty = gradient_penalty_weight * K.square(1 - gradient_l2_norm)
+    # return the mean as loss over all the batch samples
+    return K.mean(gradient_penalty)
 # ===================================
     
 # ========== Main ==========
